0756b152e7ebb0fdbeb68618bf8cf0c03796dcab
JongTae.Kim <jongtainow17@gmail.com>
2022-05-16 11:48:56 +0000

    init

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="text-center">
+      <strong style="font-size:40px">{{count}}</strong>
+       <div>
+         <button 
+          type="button" 
+          @click="count++" 
+          class="border-1 rounded bg-gray-100 p-3"
+          >
+          +증가
+          </button>
+       </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>
